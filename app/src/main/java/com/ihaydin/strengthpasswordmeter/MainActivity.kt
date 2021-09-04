dd0774749b564115e63c074a481a9c3303851529
ihaydin <ihaydin7@gmail.com>
2021-09-04 04:22:17 +0300

    Update sample

--- app/src/main/java/com/ihaydin/strengthpasswordmeter/MainActivity.kt
+++ app/src/main/java/com/ihaydin/strengthpasswordmeter/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.ihaydin.strengthpasswordmeter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
 
@@ -16,32 +16,76 @@ class MainActivity : AppCompatActivity() {
 
         val view = findViewById<StrengthPasswordView>(R.id.strengthPasswordView)
         val etContent = findViewById<EditText>(R.id.etContent)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
 
         view.setTintColor(R.color.cardview_light_background)
         view.setStrengthValue(5)
-        view.setStrengthBarSizeValue(50)
+        view.setStrengthBarSizeValue(16)
         view.setStrengthBarSpacerValue(10)
-        view.setStrengthTextHeightValue(40)
+        view.setStrengthTextHeightValue(20)
+        view.setTextPaddingStart(8f)
 
         val states = listOf(
             StrengthPasswordView.State(
-                "Zayıff",
-                ContextCompat.getColor(this, R.color.purple_500)
+                "Çok Zayıf",
+                ContextCompat.getColor(this, android.R.color.holo_red_dark)
+            ),
+            StrengthPasswordView.State(
+                "Zayıf",
+                ContextCompat.getColor(this, android.R.color.holo_red_light)
             ),
             StrengthPasswordView.State(
                 "Orta",
-                ContextCompat.getColor(this, R.color.teal_700)
+                ContextCompat.getColor(this, android.R.color.holo_orange_light)
             ),
             StrengthPasswordView.State(
                 "Güçlü",
-                ContextCompat.getColor(this, R.color.teal_200)
+                ContextCompat.getColor(this, android.R.color.holo_green_light)
+            ),
+            StrengthPasswordView.State(
+                "Çok Güçlü",
+                ContextCompat.getColor(this, android.R.color.holo_green_dark)
             )
         )
 
         view.setList(states)
 
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, value: Int, p2: Boolean) {
+                if (value != 0) {
+                    when (value) {
+                        1 -> {
+                            view.setSelection(0)
+                            view.setTintSize(1)
+                        }
+                        2 -> {
+                            view.setSelection(1)
+                            view.setTintSize(2)
+                        }
+                        3 -> {
+                            view.setSelection(2)
+                            view.setTintSize(3)
+                        }
+                        4 -> {
+                            view.setSelection(3)
+                            view.setTintSize(4)
+                        }
+                        5 -> {
+                            view.setSelection(4)
+                            view.setTintSize(5)
+                        }
+                    }
+                }
+            }
 
-        etContent.addTextChangedListener(object : TextWatcher{
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+
+        etContent.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -49,18 +93,18 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
-            override fun afterTextChanged(p0: Editable?) {
-                if (!p0.isNullOrEmpty()){
+            override fun afterTextChanged(value: Editable?) {
+                if (!value.isNullOrEmpty()) {
                     when {
-                        p0.toString() == "zay" -> {
+                        value.toString() == "zay" -> {
                             view.setSelection(0)
                             view.setTintSize(1)
                         }
-                        p0.toString() == "or" -> {
+                        value.toString() == "zayor" -> {
                             view.setSelection(1)
                             view.setTintSize(3)
                         }
-                        p0.toString()=="gü" -> {
+                        value.toString() == "zayorgü" -> {
                             view.setSelection(2)
                             view.setTintSize(5)
                         }
